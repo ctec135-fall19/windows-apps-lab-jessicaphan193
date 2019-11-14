a45143e82948f563f71b04b5b9bf3b4d32b61655
Jessica Phan <J.PHAN2@students.clark.edu>
2019-11-14 14:47:55 -0800

    In class

---
+++ Mod.8/Partial_Class/MyClass.Boilerplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class
+{
+    partial class MyClass
+    {
+        //int myfield;
+        private int myfield;
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Mod.8/Partial_Class/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class
+{
+    partial class MyClass
+    {
+        public MyClass(int x)
+        {
+            this.myfield = x;
+        }
+        public void printState()
+        {
+            Console.WriteLine("state: {0}", myfield);
+        }
+    }
+}
